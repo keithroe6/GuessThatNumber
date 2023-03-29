f3c53cee511fd86b87a88795f74bc3be1890878d
Keith Roe <kdroe1@gmail.com>
2023-03-29 11:12:11 -0400

    Defined variables at beginning of script file

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 // console.log('Hello World');   Check the link
 
 //Guess That Number
+// Define Variables Messager to be used throughout the project
+
+const enterNumText = 'Please enter a number greater than zero';
+
+// For restarting game
+let restartGame = true;
+
+//for storing the range of the number to be guessed.
+let rangeNum;
+
+// For storing the number of attempts the user has left
+let livesleft;
+
+// control-command-space to pull up emojis
+let cat = "😼";
+
+// For storing the user's guess
+let guess;
+
+// For storing the user's response to play again or not play again
+let playAgain;
+
