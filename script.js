16f37f9880cbb0559e0c5d36e417a36d87191cc3
Keith Roe <kdroe1@gmail.com>
2023-03-30 13:17:25 -0400

    Got game restart to work

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let restartGame = true;
 let rangeNum;
 
 // For storing the number of attempts the user has left
-let lives;
+let lives; 
 
 // control-command-space to pull up emojis
 let cat = "😼";
@@ -29,7 +29,6 @@ alert('Welcome to "Guess That Number!" Please click "OK" to start the game!');
 // Game restarts as long as restartGame has a value of true. Main game loop.
 while(restartGame) {
     // Ask user to enter a number to set the upper bound for the random number that will be created.
-
     rangeNum = prompt('Please enter a maximum number for the range.');
     // console.log(rangeNum);
     // Using parseInt to attempt to convert the user's response into a number value
@@ -41,29 +40,30 @@ while(restartGame) {
     while (!rangeNum || rangeNum < 1) {
         rangeNum = prompt(enterNumText);
         rangeNum = parseInt(rangeNum);
-    };
+    }
 
     // Creat the random number using the range number entered by the user
     randomNum = Math.floor(Math.random() * rangeNum) + 1;
     console.log(`randomNum`, randomNum, `rangeNum`, rangeNum);
 
     // Prompt user to enter a number of attempts (lives) allowed (e.g. the number of guesses)
-    lives = parseInt(prompt('Please enter a number of attempts allowed'))
+    lives = parseInt(prompt('Please enter a number of attempts allowed'));
 
     // Verifying the user's entry for a number of attempts allowed is greater than zero and equal to or less than the range they set
     while(!lives || lives < 1 || lives > rangeNum) {
-        lives = parseInt(prompt('Please enter a number of attempts allowed again'))
+        lives = parseInt(prompt('Please enter a number of attempts allowed again'));
 
     }
 
     // Ask the user to enter a guess in the range they set
-    guess = prompt(`Please enter a guess from 1 to ${rangeNum}. You have ${lives} lives left.`)
+    guess = prompt(`Please enter a guess from 1 to ${rangeNum}. You have ${lives} lives left.`);
 
     // Continue looping until user guesses correct number or runs out of attempts. NOTE: as loop is currently set up the BREAK keyword is run
        while (true) {
         //Displays the number when a code word is entered
         if (guess === `pickle`) {
             alert(`A helpful hint...The number is ${randomNum}`);
+            guess = prompt(`Please entern a guess from 1 to ${rangeNum}. You have ${lives} left.`);
         }
 
         // Tries to convert user's guess into a number
@@ -102,24 +102,19 @@ while(restartGame) {
         // Loop until user submits a valid response
         while (true) {
             // Check if the user's answer is no
-            if (playAgain.toUpperCase === 'N') {
+            if (playAgain.toUpperCase() == 'N') {
                 alert(`Thanks for playing!`);
                 restartGame = false;
                 break;
-            } else if (playAgain.toUpperCase === 'Y') {
+            } else if (playAgain.toUpperCase() === 'Y') {
                 // The game restarts
                 break;
             } else {
-                playAgain = prompt(`Please enter Y or N`);
+                playAgain = prompt('Please enter Y or N');
             }
-
-
-        
-
-
-       } 
+            
+       }
        
-
 };
 
 
